295d14b0998d58b1c32eb42a761629db15bf441e
Farhaan Bukhsh <farhaan@opencraft.com>
2024-04-11 02:06:15 +0530

    chore: Bumping the tutor version to support quince
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- tutor-contrib-harmony-plugin/setup.py
+++ tutor-contrib-harmony-plugin/setup.py
@@ -40,7 +40,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=["tutor>=16.0.0,<17.0.0"],
+    install_requires=["tutor>=17.0.0,<18.0.0"],
     entry_points={
         "tutor.plugin.v1": [
             "k8s_harmony = tutor_k8s_harmony_plugin.plugin",
@@ -52,7 +52,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
